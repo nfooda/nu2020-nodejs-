a05f402dd4dabdaf710982885246913936d58a5a
Nehal Fooda <49735241+nfooda@users.noreply.github.com>
2020-08-27 20:38:33 +0200

    Update onlineShop-mongoDB.js

--- onlineShop-mongoDB.js
+++ onlineShop-mongoDB.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true });
 
 const productSchema = mongoose.Schema({
-        _id: mongoose.Schema.Types.ObjectId,
-        name: String,
-        price: Number 
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  price: Number
 });
 
-const Product = mongoose.model("Product",productSchema);
+const Product = mongoose.model("Product", productSchema);
 const productbaseurl = "/products";
 
 
@@ -35,84 +35,89 @@ const productbaseurl = "/products";
 
 //get all products
 app.get(productbaseurl, function (req, res) {
-  Product.find() 
-  .exec() 
-  .then(prod => {
-      for(i=0;i<prod.length;i++)
-        console.log("Name: "+prod[i].name+", Price: "+prod[i].price);
-      res.json(products);
-  }) 
-  .catch(err => {
+  Product.find()
+    .exec()
+    .then(prod => {
+      for (i = 0; i < prod.length; i++)
+        console.log("Name: " + prod[i].name + ", Price: " + prod[i].price);
+      res.status(200).json(prod);
+    })
+    .catch(err => {
       console.log(err);
-  }); 
-  })
+      res.status(400).send(err);
+    });
+})
 
 
 
-  
+
 //get a product by id
-app.get(productbaseurl+'/:id', function (req, res) {
-  Product.findOne({_id:req.params.id})
-  .exec()
-  .then(prod => {
-      console.log("Name: "+prod.name+", Price: "+prod.price);
-      res.send(product);
-  })
-  .catch(err => {
+app.get(productbaseurl + '/:id', function (req, res) {
+  Product.findOne({ _id: req.params.id })
+    .exec()
+    .then(prod => {
+      console.log("Name: " + prod.name + ", Price: " + prod.price);
+      res.status(200).send(prod);
+    })
+    .catch(err => {
       console.log(err);
-  });
-  })
+      res.status(400).send(err);
+    });
+})
 
 
 
-  
+
 //add product to the list
 app.post(productbaseurl, function (req, res) {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price 
-  }); 
-
-  product 
-    .save() 
-    .then(result => {
-        console.log(result);
-        res.send(products);
-    }) 
-    .catch(err => {
-        console.log(err);
-    });
-  })
-
-
-  
-//delete product by id
-app.delete(productbaseurl+'/:id', function (req,res){
-  Product.remove({_id:req.params.id})
-  .exec()
-  .then(result => {
-      console.log(result);
-      res.json(products);
-  })
-  .catch(err => {
-      console.log(err);
+    price: req.body.price
   });
+
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(200).send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send(err);
+    });
+})
+
+
+
+//delete product by id
+app.delete(productbaseurl + '/:id', function (req, res) {
+  Product.remove({ _id: req.params.id })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(prod);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send(err);
+    });
 })
 
 
 
 //update product by id
-app.put(productbaseurl+'/:id', function (req,res){
-  Product.update({_id:req.params.id}, {price:100}) //that's the new price
-  .exec()
-  .then(result => {
+app.put(productbaseurl + '/:id', function (req, res) {
+  Product.update({ _id: req.params.id }, { price: 100 }) //that's the new price
+    .exec()
+    .then(result => {
       console.log(result);
-      res.json(product);
-      })
-  .catch(err => {
+      res.status(200).send(result);
+    })
+    .catch(err => {
       console.log(err);
-  });
+      res.status(400).send(err);
+    });
 })
 
 app.listen(3000)
